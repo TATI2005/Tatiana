9633f2ce27b4bcc5a23a60b268eec36e30335b84
TATI2005 <141774557+TATI2005@users.noreply.github.com>
2025-03-20 13:33:28 -0500

    Add files via upload

---
+++ tienda.sql
@@ -0,0 +1,272 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 20-03-2025 a las 19:07:42
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tienda`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `mostrar` ()   begin
+    select * from producto;
+end$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado_producto`
+--
+
+CREATE TABLE `estado_producto` (
+  `id_estado` int(11) NOT NULL,
+  `id_producto` int(11) DEFAULT NULL,
+  `estado` varchar(50) DEFAULT NULL,
+  `fecha` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `estado_producto`
+--
+
+INSERT INTO `estado_producto` (`id_estado`, `id_producto`, `estado`, `fecha`) VALUES
+(0, 1, 'excelente', '2024-02-11');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id_producto` int(11) NOT NULL,
+  `producto` varchar(50) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `fecha` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`id_producto`, `producto`, `cantidad`, `fecha`) VALUES
+(1, 'arroz', 9, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `id_proveedor` int(11) NOT NULL,
+  `proveedor` varchar(50) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `fecha` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `proveedor`
+--
+
+INSERT INTO `proveedor` (`id_proveedor`, `proveedor`, `cantidad`, `fecha`) VALUES
+(1, 'Juan', 12, '2024-02-01');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `usuario` varchar(50) DEFAULT NULL,
+  `correo` varchar(50) DEFAULT NULL,
+  `direccion` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `usuario`, `correo`, `direccion`) VALUES
+(1, 'carla', 'car12@hotmail.com', 'santo domingo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `id_venta` int(11) NOT NULL,
+  `venta` varchar(50) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `venta`
+--
+
+INSERT INTO `venta` (`id_venta`, `venta`, `cantidad`) VALUES
+(1, 'arroz', 12);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura Stand-in para la vista `vista_pproducto`
+-- (Véase abajo para la vista actual)
+--
+CREATE TABLE `vista_pproducto` (
+`proveedor` varchar(50)
+,`cantidad` int(11)
+,`producto` varchar(50)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura Stand-in para la vista `vista_venta`
+-- (Véase abajo para la vista actual)
+--
+CREATE TABLE `vista_venta` (
+`id_usuario` int(11)
+,`venta` varchar(50)
+,`cantidad` int(11)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura Stand-in para la vista `vista_vproducto`
+-- (Véase abajo para la vista actual)
+--
+CREATE TABLE `vista_vproducto` (
+`id_producto` int(11)
+,`venta` varchar(50)
+,`cantidad` int(11)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura para la vista `vista_pproducto`
+--
+DROP TABLE IF EXISTS `vista_pproducto`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vista_pproducto`  AS SELECT `proveedor`.`proveedor` AS `proveedor`, `proveedor`.`cantidad` AS `cantidad`, `producto`.`producto` AS `producto` FROM (`proveedor` join `producto` on(`proveedor`.`id_proveedor` = `producto`.`id_producto`)) ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura para la vista `vista_venta`
+--
+DROP TABLE IF EXISTS `vista_venta`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vista_venta`  AS SELECT `usuario`.`id_usuario` AS `id_usuario`, `venta`.`venta` AS `venta`, `venta`.`cantidad` AS `cantidad` FROM (`usuario` join `venta` on(`usuario`.`id_usuario` = `venta`.`id_venta`)) ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura para la vista `vista_vproducto`
+--
+DROP TABLE IF EXISTS `vista_vproducto`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vista_vproducto`  AS SELECT `producto`.`id_producto` AS `id_producto`, `venta`.`venta` AS `venta`, `venta`.`cantidad` AS `cantidad` FROM (`producto` join `venta` on(`producto`.`id_producto` = `venta`.`id_venta`)) ;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `estado_producto`
+--
+ALTER TABLE `estado_producto`
+  ADD PRIMARY KEY (`id_estado`),
+  ADD KEY `id_producto` (`id_producto`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id_producto`);
+
+--
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`id_proveedor`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`id_venta`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  MODIFY `id_proveedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `id_venta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `estado_producto`
+--
+ALTER TABLE `estado_producto`
+  ADD CONSTRAINT `estado_producto_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
